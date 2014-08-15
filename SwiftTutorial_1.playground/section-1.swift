5e04a9a2323ed4aef2ea9912ff2d88c5a0674d48
55011212092Boy <iMac@msu>
2014-08-15 16:48:48 +0700

    Test

---
+++ SwiftTutorial_1.playground/section-1.swift
@@ -0,0 +1,28 @@
+let tutorialTeam = 56
+let editoriakTeam = 23
+var totalTeam = tutorialTeam + editoriakTeam
+
+totalTeam += 1
+let priceIntInferred = -19
+let priceIntExplicit:Int = -19
+
+let priceInferred = -19.99
+let priceExplicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+if (onSaleInferred) {
+    println("\(nameInferred) on sale for \(priceInferred)!")
+} else {
+    println("\(nameInferred) at regular price: \(priceInferred)!")
+}
+
+
