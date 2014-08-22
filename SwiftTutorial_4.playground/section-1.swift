2fb10762107ee3886f8027d1ba779ded37b65539
55011212182som <imac@msu>
2014-08-22 15:16:58 +0700

    AddRemoveDictionary

--- SwiftTutorial_4.playground/section-1.swift
+++ SwiftTutorial_4.playground/section-1.swift
@@ -13,6 +13,29 @@ print("The airports dictionary contains \(airports.count) items.")
 //print "The airports dictionary contains 2 items."
 
 
+//add
+airports["LHR"] = "London Heathrow"
+//the value for "LHR" has been changed to "London Heathow"
+
+
+if let oldValue = airports.updateValue("Dublin International", forKey: "DUB"){
+    println("The old value for DUB was \(oldValue).")
+}
+// prints "The old value for DUB was Dublin."
+
+//remove
+airports["APL"] = "Apple International"
+    //"Apple International" is not the real airport for APL, so delete it
+    airports["APL"] = nil
+    //APL has now been remove from the dictionary
+    
+if  let removedValue = airports.removeValueForKey("DUB") {
+    println("The removed airport's name is \(removedValue).")
+} else {
+    println("The airports dictionary dose not contain a value for DUB.")
+}
+//print "The remove airport's name is Dublin International.
+
 
 
 
