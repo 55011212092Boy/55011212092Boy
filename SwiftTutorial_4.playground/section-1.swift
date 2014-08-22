fa8e7ab7b7a0f548f734c34772bac0e26554a916
55011212182som <imac@msu>
2014-08-22 15:10:30 +0700

    Dictionary

--- SwiftTutorial_4.playground/section-1.swift
+++ SwiftTutorial_4.playground/section-1.swift
@@ -1,5 +1,21 @@
-// Playground - noun: a place where people can play
+let array: Array<Int> = [1,2,3,4]
+let dictionare: Dictionary<String,Int> = ["dog":1,"elephamt":2]
 
-import UIKit
+var airports: [String:String] = ["TYO":"Tokyo","DUB":"Dublin"]
+if airports.isEmpty {
+    print("The airports dictionary is empty")
+} else {
+    print("The airports dictionary is not empty")
+}
+//print "The airports dictionary is not empty"
 
-var str = "Hello, playground"
+print("The airports dictionary contains \(airports.count) items.")
+//print "The airports dictionary contains 2 items."
+
+
+
+
+
+
+
+
