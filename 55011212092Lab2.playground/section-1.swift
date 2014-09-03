2f83ecedb79edfd1f08359f1d33c9f2de908583b
55011212111viw <imac@msu>
2014-09-03 15:22:29 +0700

    aruAni

--- 55011212092Lab2.playground/section-1.swift
+++ 55011212092Lab2.playground/section-1.swift
@@ -1,15 +1,36 @@
 class animal{
     var name:[String:String]
+    //var nameDic:Dictionary<String,String>
     
-    init(name:[String:String]){
+    
+    init(name:[String:String]/*,nameDic:Dictionary<String,String>*/){
         self.name = name
+        //self.nameDic = nameDic
     }
+    
     func addAnimal()->[String:String]{
         name["Monkey"] = "Banana"
-        name["Cat"] = "Grass"
-        name["Chicken"] = "Rice"
         return name
     }
+    func removeAnimal(){
+       let rm = name.removeValueForKey("Sheep");
+            println("remove name \(rm).")
+    }
+    func updateAnimal(){
+        let ud = name.updateValue("Fish", forKey: "Cat" )
+    }
+    /*func addAni(name:Dictionary<String,String>){
+        for (key,value) in name {
+            nameDic[key] = value
+            println("key: \(key)  value:\(value)")
+        }
+    }*/
 }
-let add = animal(name: ["Sheep":"Corn",])
+let add = animal(name:["Sheep":"Corn","Bird":"Rice","Tiger":"Beef","Chicken":"Rice","Cat":"Grass"])
+
 add.addAnimal()
+
+
+
+
+
